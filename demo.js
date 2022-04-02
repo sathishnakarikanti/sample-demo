5e4f4c5c1ea624b850abb5abbd01bafb2f973e15
sathish <sathishnakarikantig@gmail.com>
2022-04-02 19:46:03 +0530

    added gitignore file

---
+++ demo.js
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Git Demo</title>
+        <script>
+
+        </script>
+    </head>
+    <body>
+        <h1>GITHUB</h1>
+        <p>Hai sathish bhai</p>
+    </body>
+</html>
